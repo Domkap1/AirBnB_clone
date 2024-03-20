449bed88a752b338280c4b0e8f43b993c4e261c6
Thabisile <thabisilemkhize81@gmail.com>
2024-03-20 13:32:00 -0700

    City

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the city class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """The city class."""
+    state_id: str = ""
+    name: str = ""
