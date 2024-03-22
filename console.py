2a8e9c347917971fd97cbea453c9fc3f8ce5b586
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-03-22 17:38:53 +0000

    Update console.py

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Defines the HBNB console."""
 import cmd
 from models.base_models import BaseModel
+from models.engine.file_storage import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +23,66 @@ def do_EOF(self, arg):
   """Exit the command interpreter on EOF."""
   print()
   exit()
+
+def do_create(self, args):
+  """Creates new instance of base model."""
+  args = args.split()
+  if not args:
+    print("** class name missing **")
+    return
+  class_name = args[0]
+  if class_name not in storage.all().keys()
+  print("** class doesn't exist **")
+  return
+  new_obj = storage.all()[class_namce]()
+  new_obj.save()
+  print(f"{new_obj.id}")
+
+def do_show(self, args):
+  """Print string rep of an instance."""
+  args = args.split()
+  if not args:
+    print("** class name missing **")
+    return
+  class_name = args[0]
+  if class_name not in storage.all().keys():
+    print("** class doesn't exist **")
+    return
+  if len(args) < 2:
+    print("** instance id missing **")
+    return
+    obj_id = args[1]
+    obj = storage.get(class_name,obj_id)
+    if not obj:
+      print("** no instance found **")
+    else:
+      print(obj)
+
+def do_destroy(self, args):
+  """Deletes an instance."""
+  args = args.split()
+  obj = storage.get(class_name,obj_id)
+  if not obj:
+    print("** no instance found **")
+  else:
+    storage.delete(obj)
+    storage.save()
+    print(f"{class_name} {obj_id} deleted")
+
+def do_all(self, args):
+  """Prints all string rep of instance."""
+  args = args.split()
+  if args:
+    class_name = args[0]
+    if class_name not in storage.all().keys():
+      print("** class doesn't exist **")
+      return
+    instances = storage.all(class_name)
+  else:
+    instances = storage.all()
+  all_strings = [str(instance) for instance in instances.values()]
+  print(all_strings)
+
 
 if __name__ == '__main__':
   HBNBCommand().cmdloop()
