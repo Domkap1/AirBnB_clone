d916ecee1fc59c49fdc2e5e42a0b72b2f51a7c5d
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-03-25 06:21:29 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -5,6 +5,6 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """Review class."""
-    place_id: str = ""
-    user_id: str = ""
-    text: str = ""
+    place_id = ""
+    user_id = ""
+    text = ""
